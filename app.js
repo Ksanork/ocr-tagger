4b10faea4088c14aa06e75df08150dc61b0b1021
Ksanork <miki11@poczta.onet.pl>
2018-03-12 19:33:26 +0100

    Test ECMAScript 6

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const app = express();
-//
-// app.get('/', (req, res) => res.send('Hello World!'));
-//
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const express = require('express');
+const app = express();
 
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello world!\n");
-});
-server.listen(3000);
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end("hello world!\n");
+// });
+// server.listen(3000);
