c239d13cb58f790fb2d7811a3b5adf65ae452bdf
Ksanork <miki11@poczta.onet.pl>
2018-03-12 19:47:42 +0100

    Test multiple repos

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('DatasetTagger!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end("hello world!\n");
-// });
-// server.listen(3000);
