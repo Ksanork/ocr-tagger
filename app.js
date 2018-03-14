4a31dd4ec7ed9e0e504448fc9c6b42ee4fb021ee
Ksanork <miki11@poczta.onet.pl>
2018-03-14 13:34:05 +0100

    Add view engine

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('DatasetTagger!'));
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => res.render("login"));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
