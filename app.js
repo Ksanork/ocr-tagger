de3a89c66ae34bd42443210f0fc65bf32e534157
Ksanork <miki11@poczta.onet.pl>
2018-03-14 14:20:32 +0100

    Test static resources

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.render("login"));
+app.get('/', (req, res) => res.render("main"));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('Example app listening on port 3000'));
