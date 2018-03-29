d5c20a9c8a9c2d076298feb4046442fbc4022be9
Ksanork <miki11@poczta.onet.pl>
2018-03-29 19:42:52 +0200

    Configure dev

--- dist/src/config.js
+++ dist/src/config.js
@@ -5,6 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 var config = {
     'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt"
+
 };
 
 exports.default = config;
