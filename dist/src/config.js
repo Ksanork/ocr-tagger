27871c7fe367270e97aeca1d4dd5562e32684e30
Ksanork <miki11@poczta.onet.pl>
2018-03-29 19:48:45 +0200

    Configure dev

--- dist/src/config.js
+++ dist/src/config.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var config = {
-    'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt"
-
+    'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt",
+    'tekst': "?"
 };
 
 exports.default = config;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt"
-
+    'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt",
+    'tekst': "?"
 };
 
 export default config;
