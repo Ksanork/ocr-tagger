76858f45090974405e78f8704702bb268220e110
Ksanork <miki11@poczta.onet.pl>
2018-03-29 19:18:27 +0200

    Configure dev

--- dist/src/config.js
+++ dist/src/config.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var config = {
-    'mongo_url': "mongodb://localhost:27017/dataset_tagger"
+    'mongo_url': "mongodb://mo1162_dev_dt:m99NlZTXoTpC5BjfWH34@mongo23.mydevil.net:27017/mo1162_dev_dt"
 };
 
 exports.default = config;
